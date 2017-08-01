876f0619a0b5afe1efc3cba5ea1e9652c047d296
Kevin Coleman <kevinjamescoleman.7@gmail.com>
2017-08-01 11:59:31 -0700

    Add more InSpec tests.

--- cookbooks/webserver_test/recipes/default.rb
+++ cookbooks/webserver_test/recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Install the Apache package.
+package 'httpd'
+
+service 'httpd' do
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+  content '<html>
+  <body>
+    <h1>hello world</h1>
+  </body>
+</html>'
+end
